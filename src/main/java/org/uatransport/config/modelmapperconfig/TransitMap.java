d1dc49fdb9487d3d97fff64a268c92c5cf6062c5
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-07-01 18:15:45 +0300

    Fix for cycle and add missing last stop in routeName in some transits

--- src/main/java/org/uatransport/config/modelmapperconfig/TransitMap.java
+++ src/main/java/org/uatransport/config/modelmapperconfig/TransitMap.java
@@ -27,13 +27,16 @@ public class TransitMap implements Converter<Transit, TransitDTO> {
         //TODO: rewrite search
         if (!stops.isEmpty()) {
             firstStop = stops.get(0).getStreet();
-            for (int i = 0; i < stops.size(); i++) {
+            for (int i = 0; i < stops.size() - 1; i++) {
                 if (stops.get(i).getStreet().equals(stops.get(i+1).getStreet())
                     && stops.get(i).getDirection() != stops.get(i+1).getDirection()) {
                     lastStop = stops.get(i).getStreet();
                     found = true;
                 }
                 if (found) break;
+            }
+            if (!found) {
+                lastStop = stops.get(stops.size() / 2).getStreet();
             }
             destination.setRouteName(firstStop + " - " + lastStop);
         } else {
