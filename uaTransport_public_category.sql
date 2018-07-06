8956c4a0d113800f2e21fe148967d9d5ece93ce6
Pavlo Chornyy <pawlochornyj01@gmail.com>
2018-07-06 11:34:03 +0300

    change data.sql

---
+++ uaTransport_public_category.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.category (type, id, icon_url, name, next_level_category_id) VALUES ('EXTENDABLE', 1, 'static/bus.png', 'Public Transport', null);
+INSERT INTO public.category (type, id, icon_url, name, next_level_category_id) VALUES ('EXTENDABLE', 8, 'static/taxi.png', 'Taxi', null);
+INSERT INTO public.category (type, id, icon_url, name, next_level_category_id) VALUES ('EXTENDABLE', 2, 'static/lviv.png', 'Lviv', 1);
+INSERT INTO public.category (type, id, icon_url, name, next_level_category_id) VALUES ('EXTENDABLE', 3, 'static/kyiv.png', 'Kyiv', 1);
+INSERT INTO public.category (type, id, icon_url, name, next_level_category_id) VALUES ('NON_EXTENDABLE', 4, 'static/tram.png', 'Tram', 2);
+INSERT INTO public.category (type, id, icon_url, name, next_level_category_id) VALUES ('NON_EXTENDABLE', 5, 'static/trolleybus.png', 'Trolleybus', 2);
+INSERT INTO public.category (type, id, icon_url, name, next_level_category_id) VALUES ('NON_EXTENDABLE', 6, 'static/bigbus.png', 'Bus', 2);
+INSERT INTO public.category (type, id, icon_url, name, next_level_category_id) VALUES ('NON_EXTENDABLE', 7, 'static/van.png', 'Marshrutka', 2);
