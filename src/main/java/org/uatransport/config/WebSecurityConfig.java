bec7633f4c74557b3dd8ff6a2ce12e4d72a49bda
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-07-06 11:50:44 +0300

    Set X-Frame-Options to sameOrigin

--- src/main/java/org/uatransport/config/WebSecurityConfig.java
+++ src/main/java/org/uatransport/config/WebSecurityConfig.java
@@ -34,6 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll().anyRequest().authenticated();
 
         http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+        http.headers().frameOptions().sameOrigin();
     }
 
     @Bean
