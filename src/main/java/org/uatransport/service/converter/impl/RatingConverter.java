f42a6dfc9003c22f7988f241ee85114c5eaacab2
Pavlo Chornyy <pawlochornyj01@gmail.com>
2018-07-09 13:57:39 +0300

    change RatingConverter to calculating value in range (0,10)

--- src/main/java/org/uatransport/service/converter/impl/RatingConverter.java
+++ src/main/java/org/uatransport/service/converter/impl/RatingConverter.java
@@ -10,15 +10,32 @@ import org.uatransport.service.converter.model.RatingFeedback;
 import java.util.List;
 
 public class RatingConverter implements ConversionStrategy<Double> {
+    private static final Integer MAX_ANSWER_VALUE = 10;
 
     @Override
     @SneakyThrows
     public Double convert(Feedback feedback) {
-        List<RatingFeedback> answers = new ObjectMapper().readValue(feedback.getAnswer(), new TypeReference<List<RatingFeedback>>() {});
+        List<RatingFeedback> answers = new ObjectMapper().readValue(feedback.getAnswer(), new TypeReference<List<RatingFeedback>>() {
+        });
 
+        return calculateRating(answers);
+    }
+
+    private Double calculateMaxValue(List<RatingFeedback> answers) {
+        return answers.stream()
+            .mapToInt(answer -> MAX_ANSWER_VALUE * answer.getWeight())
+            .average()
+            .orElse(0.0);
+    }
+
+    private Double calculateValue(List<RatingFeedback> answers) {
         return answers.stream()
             .mapToInt(answer -> answer.getAnswer() * answer.getWeight())
             .average()
             .orElse(0.0);
+    }
+
+    private Double calculateRating(List<RatingFeedback> answers) {
+        return (10 * calculateValue(answers)) / calculateMaxValue(answers);
     }
 }
