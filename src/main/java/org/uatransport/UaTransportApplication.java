be31fe150f6ec767bc01848c8a749ae12a73db59
OleksiiDemidov <demidov.a@me.com>
2018-07-12 04:03:46 +0300

    Disable CGlib Proxy

--- src/main/java/org/uatransport/UaTransportApplication.java
+++ src/main/java/org/uatransport/UaTransportApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
 
 //@EntityScan(
 //    basePackageClasses = {UaTransportApplication.class, Jsr310JpaConverters.class}
@@ -14,7 +15,7 @@ import org.springframework.cache.annotation.EnableCaching;
 public class UaTransportApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UaTransportApplication.class, args);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(UaTransportApplication.class, args);
         log.debug("--Application Started--");
     }
 }
