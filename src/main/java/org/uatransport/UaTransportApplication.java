15073051fa05f1d67c10b55e8062f631302abdbc
Iryna Holod <ira_ih@ukr.net>
2018-07-12 18:24:50 +0300

    add data sql

--- src/main/java/org/uatransport/UaTransportApplication.java
+++ src/main/java/org/uatransport/UaTransportApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
 
 //@EntityScan(
 //    basePackageClasses = {UaTransportApplication.class, Jsr310JpaConverters.class}
@@ -14,7 +15,15 @@ import org.springframework.cache.annotation.EnableCaching;
 public class UaTransportApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UaTransportApplication.class, args);
+        ConfigurableApplicationContext context=SpringApplication.run(UaTransportApplication.class, args);
         log.debug("--Application Started--");
     }
+
+   /* List<Feedback> feedbacks= context.getBean(FeedbackService.class).getByUserId(1);
+    List<Transit> transits = new ArrayList<>();
+for (int i = 0; i < feedbacks.size(); i++) {
+        transits.add(feedbacks.get(i).getTransit());
+    }*/
+   /*Optional<Transit> transit =  context.getBean(TransitRepository.class).findById(35);
+context.getBean(ModelMapper.class).map(transit,TransitDTO.class);*/
 }
